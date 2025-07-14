dbe77be939659c7d9296b789573813e187c11827
yygqzzk <1374174364@qq.com>
2025-07-15 01:12:19 +0800

    feat: 分布式缓存实现并发访问

---
+++ cache.go
@@ -0,0 +1,39 @@
+package gcache
+
+import (
+	"sync"
+
+	"github.com/yygqzzk/gCache/lru"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+// 添加缓存
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	// 延迟初始化
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (_ ByteView, _ bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		return
+	}
+
+	if value, ok := c.lru.Get(key); ok {
+		return value.(ByteView), ok
+	}
+
+	return
+}
