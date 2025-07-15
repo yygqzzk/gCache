9647dc6e36761c035e1c12c8f6ae4639358072aa
yygqzhu <yygqzhu@tencent.com>
2025-07-15 15:47:57 +0800

    feat: 分布式缓存实现并发访问

--- gcache.go
+++ gcache.go
@@ -1,5 +1,11 @@
 package gcache
 
+import (
+	"fmt"
+	"sync"
+)
+
+// Getter 接口，当缓存不存在时，调用该接口获取数据
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
@@ -8,4 +14,75 @@ type GetterFunc func(key string) ([]byte, error)
 
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+// Group 是缓存的命名空间，每个 Group 拥有一个唯一的名称。
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:   name,
+		getter: getter,
+		mainCache: cache{
+			cacheBytes: cacheBytes,
+		},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.Unlock()
+	if g, exists := groups[name]; exists {
+		return g
+	}
+	return nil
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+// 将数据添加到缓存中
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
